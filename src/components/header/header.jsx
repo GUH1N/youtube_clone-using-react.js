f6b2cf59453f037ca2df7440b2a8d0c6b277dcbb
GUH1N <erodeguhaapriyan@gmail.com>
2024-09-02 21:15:54 +0530

    code

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,33 +1,50 @@
 import React from "react";
 import "./header.css";
-import logo from "../youtube-logo.png";
+import { FaYoutube, FaSearch, FaMicrophone, FaCamera, FaBell, FaUserCircle } from "react-icons/fa";
 
 export default function Header() {
     return (
-        <div className="container">
-        <div className="side_container">
-            <ul className="list_container">
-                <li className="item-1"><i class="fa-solid fa-bars"></i></li>
-                <li className="item"><div className="it1"><i class="fa-solid fa-house"></i>
-                    <p>Home</p></div></li>
-                <li className="item"><div  className="it2"><i class="fa-solid fa-compact-disc"></i>
-                    <p>Shorts</p></div></li>
-                <li className="item"><div  className="it3"><i class="fa-regular fa-folder-open"></i>
-                    <p>Subscriptions</p></div></li>
-                <li className="item"><div  className="it4"><i class="fa-solid fa-user-tag"></i>
-                    <p>You</p></div></li>
-            </ul>
-        </div>
-            <div className="youtube_con">
-                    <div id="youtube">
-                        <i id="Youtube_icon" class="fa-brands fa-youtube"></i>
-                        <p>YouTube</p>
+        <div className="element">
+            <div className="maintag">
+                <div className="sections">
+                    <div className="icon">
+                        <i class="fa-solid fa-bars"></i>
                     </div>
-                    <div id="search">
-                        <input placeholder="Search"></input>
-                        <i id="magnifier" class="fa-solid fa-magnifying-glass"></i>
+                    <div className="icon">
+                        <i class="fa-solid fa-house"></i>
+                        <p>Home</p>
+                    </div>
+                    <div className="icon">
+                        <i class="fa-solid fa-compact-disc"></i>
+                        <p>Shorts</p>
+                    </div>
+                    <div className="icon">
+                        <i class="fa-regular fa-folder-open"></i>
+                        <p>Subscription</p>
+                    </div>
+                    <div className="icon">
+                        <i class="fa-solid fa-user-tag"></i>
+                        <p>You</p>
                     </div>
                 </div>
+            </div>
+            <div className="section1">
+                <div className="searchbox">
+                    <div className="split1">
+                        <h1 className="headh1"><FaYoutube /> YouTube</h1>
+                    </div>
+                    <div className="split2">
+                        <input type="text" placeholder="Search" />
+                        <FaSearch />
+                        <FaMicrophone />
+                    </div>
+                    <div className="split3">
+                        <FaCamera />
+                        <FaBell />
+                        <FaUserCircle />
+                    </div>
+                </div>    
+            </div>
         </div>
     );
 }
